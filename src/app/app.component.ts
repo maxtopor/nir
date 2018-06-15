5d4a9f9911f1fd1dad11c039aa31b8121bd8a980
sansarayo <Maksim_Taparenka@epam.com>
2018-06-15 10:30:42 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Pipe, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { DataService } from './core/services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  constructor(private dataService: DataService, private router: Router){}
+  ngOnInit(): void {
+    if (localStorage.getItem("token")){
+      this.dataService.checkToken().subscribe(data=>{
+        if (data.status=="ERROR")
+          this.router.navigate(["login"]);
+        else
+          this.router.navigate([""]);  
+      });
+    }
+    else
+      this.router.navigate(["login"]);
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,78 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+
+import { MatIconModule, MatButtonModule, MatInputModule } from '@angular/material/'
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { DataService } from './core/services/data.service';
+import { AppPipe } from './app.pipe';
+import { HomeComponent } from './core/components/home/home.component';
+import { LoginComponent } from './core/components/login/login.component';
+
+
+
+import { HomePipe } from './core/components/home/home.pipe';
+import { ColumnPipe } from './core/components/home/column.pipe';
+import { TokenInterceptor } from './token.interceptor';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    AppPipe,
+    HomePipe,
+    ColumnPipe
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+    FormsModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+    
   ],
-  providers: [],
+  providers: [
+    DataService,
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:TokenInterceptor,
+      multi:true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(router : Router){
+    //console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
+}
